ecc98915f4d8f0bf63db05b43056025d9743c0b0
Amrita Mishra <itizamritamishra@gmail.com>
2025-11-22 13:16:12 +0530

    update

---
+++ catelog/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Product, SKU
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = "__all__"
+
+class SKUSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SKU
+        fields = "__all__"
